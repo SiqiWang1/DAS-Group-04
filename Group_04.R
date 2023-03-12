b8ede222fc087e8648a0dce5c2321bded5f17a22
litingwangg <wangliting0630@191email.com>
2023-03-12 17:29:26 +0000

    Modified content: 1. Simplify the column names to make the plot graph scale better and adjust the following column names. 2. Add the Goodness-of-fit test. 3. Add the OR and confidence interval.

--- Group_04.R
+++ Group_04.R
@@ -27,10 +27,20 @@ hist(household$Total.Number.of.Family.members)
 
 #################### Plot the Correlation Matrix ####################
 
-# get all numerical variables to check on the Correlation
-hosehold_num <- household[, sapply(household, is.numeric)]
+# Simplified column names 
+colnames(household)[1]<-"Income"             # original name "Total.Household.Income" 
+colnames(household)[3]<-"FoodExp"            # original name "Total.Food.Expenditure"   
+colnames(household)[4]<-"Householder_Sex"    # original name "Household.Head.Sex"
+colnames(household)[5]<-"Householder_Age"    # original name "Household.Head.Age"
+colnames(household)[6]<-"Household_Type"     # original name "Type.of.Household"
+colnames(household)[7]<-"Number_Members"     # original name "Total.Number.of.Family.members"
+colnames(household)[8]<-"Floorarea"          # original name "House.Floor.Area" 
+colnames(household)[10]<-"Number_bedrooms"   # original name "Number.of.bedrooms"
 
-cor_matrix <- cor(hosehold_num) %>% #create the correlation matrix of variables
+# get all numerical variables to check on the Correlation
+household_num <- household[, sapply(household, is.numeric)]
+
+cor_matrix <- cor(household_num) %>% #create the correlation matrix of variables
   round(2)
 cor_matrix
 
@@ -42,53 +52,52 @@ ggpairs(hosehold_num)
 # hosehold_cat <- household %>%
 #   select("Electricity","Household.Head.Sex","Type.of.Household")
 # 
-# scaled_vars <- data.frame(scale(hosehold_num)) %>%
+# scaled_vars <- data.frame(scale(household_num)) %>%
 #   select(-Total.Number.of.Family.members)
 # 
-# scaled_household <- cbind(hosehold_cat,scaled_vars,household$Total.Number.of.Family.members)
+# scaled_household <- cbind(household_cat,scaled_vars,household$Total.Number.of.Family.members)
 
 
 ###################       Boxplot      ###################
-p1<-ggplot(data = household, mapping = aes(x = Household.Head.Sex, y = Total.Number.of.Family.members)) +
-  geom_boxplot(aes(fill =Household.Head.Sex ) )+
-  labs(x = "Sex", y = "Total.Number.of.Family.member",
+p1<-ggplot(data = household, mapping = aes(x = Householder_Sex, y = Number_Members)) +
+  geom_boxplot(aes(fill =Householder_Sex ) )+
+  labs(x = "Householder_Sex", y = "Number_Members",
        title = "gender")  + 
   scale_x_discrete(labels = c("female","male"))
 
-p2<-ggplot(data = household, mapping = aes(x = Type.of.Household, y = Total.Number.of.Family.members)) +
-  geom_boxplot(aes(fill =Type.of.Household ) )+
-  labs(x = "tyoe of household", y = "Total.Number.of.Family.member",
-       title = "type")  + 
-  scale_x_discrete(labels = c("extend","single","two or more"))
+p2<-ggplot(data = household, mapping = aes(x = Household_Type, y = Number_Members)) +
+  geom_boxplot(aes(fill = Household_Type))+
+  labs(x = "Household_Type", y = "Number_Members",title = "type") +
+  scale_x_discrete(labels = c("Extended", "Single", "Two or More"))
 
-p3<-ggplot(data = household, mapping = aes(x = Electricity, y = Total.Number.of.Family.members)) +
+p3<-ggplot(data = household, mapping = aes(x = Electricity, y = Number_Members)) +
   geom_boxplot(aes(fill =Electricity ) )+
-  labs(x = "Electricity", y = "Total.Number.of.Family.member",
+  labs(x = "Electricity", y = "Number_Members",
        title = "Electricity")  + 
   scale_x_discrete(labels = c("no","yes"))
 
-grid.arrange(p1, p2, p3, nrow=2, ncol = 2)
+grid.arrange(p1, p2, p3, nrow=3, ncol = 1)
 
 ###################   check on categorical date    ###################
 household %>% 
-  tabyl(Total.Number.of.Family.members,Household.Head.Sex) %>% 
+  tabyl(Number_Members, Householder_Sex) %>% 
   adorn_percentages() %>% 
   adorn_pct_formatting() %>% 
   adorn_ns() # To show original counts
 
 ###################        plot of histogram       ###################
-# barplot of Total.Number.of.Family.members and Household.Head.Sex
-H1 <- ggplot(household, aes(x= Total.Number.of.Family.members,  y = ..prop.., group=Household.Head.Sex, fill=Household.Head.Sex)) + 
+# barplot of Number_Members and Householder_Sex
+H1 <- ggplot(household, aes(x= Number_Members,  y = ..prop.., group=Householder_Sex, fill=Householder_Sex)) + 
   geom_bar(position="dodge", stat="count") +
   labs(y = "Proportion")
 
-# barplot of Total.Number.of.Family.members and Electricity 
-H2 <- ggplot(household, aes(x= Total.Number.of.Family.members,  y = ..prop.., group=Electricity, fill=Electricity)) + 
+# barplot of Number_Members and Electricity 
+H2 <- ggplot(household, aes(x= Number_Members,  y = ..prop.., group=Electricity, fill=Electricity)) + 
   geom_bar(position="dodge", stat="count") +
   labs(y = "Proportion")
 
-# barplot of Total.Number.of.Family.members and Type.of.Household
-H3 <- ggplot(household, aes(x= Total.Number.of.Family.members,  y = ..prop.., group=Type.of.Household, fill=Type.of.Household)) + 
+# barplot of Number_Members and Household_Type
+H3 <- ggplot(household, aes(x= Number_Members,  y = ..prop.., group=Household_Type, fill=Household_Type)) + 
   geom_bar(position="dodge", stat="count") +
   labs(y = "Proportion")
 
@@ -96,21 +105,21 @@ grid.arrange(H1, H2, H3, ncol=1)
 
 ###################   model fitting   ###################
 
-m1 <- glm(formula = Total.Number.of.Family.members ~ Total.Household.Income + Total.Food.Expenditure + 
-      Household.Head.Sex + Household.Head.Age + Type.of.Household + House.Floor.Area +
-      House.Age + Number.of.bedrooms + Electricity, 
-    family = poisson(link = "log"), data = household)
+m1 <- glm(formula = Number_Members ~ Income + FoodExp + Householder_Sex + 
+            Householder_Age + Household_Type + Floorarea +
+            House.Age + Number_bedrooms + Electricity, 
+          family = poisson(link = "log"), data = household)
 
-m2 <- glm(formula = Total.Number.of.Family.members ~ Total.Food.Expenditure, family = poisson(link = "log"), data = household)
+m2 <- glm(formula = Number_Members ~ FoodExp, family = poisson(link = "log"), data = household)
 
 summary(m1)
 summary(m2)
 
 
 # Use BIC to do variable selection
-output <- bic.glm(Total.Number.of.Family.members ~ Total.Household.Income + Total.Food.Expenditure + 
-                     Household.Head.Sex + Household.Head.Age + Type.of.Household + House.Floor.Area +
-                     House.Age + Number.of.bedrooms + Electricity, 
+output <- bic.glm(Number_Members ~ Income + FoodExp + Householder_Sex + 
+                    Householder_Age + Household_Type + Floorarea +
+                    House.Age + Number_bedrooms + Electricity, 
                    glm.family = "poisson" , data = household)
 summary(output)
 
@@ -120,6 +129,20 @@ m1.aic <- step(m1)
 # Use the StepAIC function to perform a stepwise regression
 # step.model.b <- stepAIC(m1, direction = "both")
 # summary(step.model.b)
+
+
+# Goodness-of-fit test
+chisq <- with(m1, sum((household$Number_Members- fitted.values)^2/fitted.values))
+df <- with(m1, df.residual)
+p <- with(m1, pchisq(chisq, df, lower.tail = FALSE))
+cat("Chi-square test statistic = ", chisq, "\n")
+cat("df = ", df, "\n")
+cat("p-value = ", p, "\n")
+
+# The coef() function obtains the coefficients of the model.
+# The confint() function obtains the confidence interval of the model coefficients.
+exp(cbind(OR = coef(m1), confint(m1)))
+
 
 
 
